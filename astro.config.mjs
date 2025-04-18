16036803a156cca147f6fecb969b14af608679f9
Lorezz <point.point@gmail.com>
2025-04-18 09:03:22 +0200

    feat: decapcms github oauth provider

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,22 @@
 import { defineConfig } from "astro/config";
 import { resolve } from "path";
-
 import react from "@astrojs/react";
+import decapCmsOauth from "astro-decap-cms-oauth";
 
+// const sveltia = {
+//   decapCMSSrcUrl: "https://unpkg.com/@sveltia/cms/dist/sveltia-cms.js",
+// };
 // https://astro.build/config
 export default defineConfig({
   vite: {
     resolve: {
       alias: {
         "@splidejs/splide/src/css/core/index": resolve(
-          "node_modules/@splidejs/splide/src/css/core/index.scss"
+          "node_modules/@splidejs/splide/src/css/core/index.scss",
         ),
       },
     },
   },
 
-  integrations: [react()],
+  integrations: [react(), decapCmsOauth()],
 });
